0037387ec588991a8fe820c9487ca6804550159d
unknown <sonthoren@gmail.com>
2021-08-12 17:51:40 +0700

    hinh_chu_nhat_1

---
+++ vehinhchunhat.py
@@ -0,0 +1,23 @@
+import turtle
+
+t = turtle.Turtle()
+
+m = 100;
+n = 150;
+
+
+t.forward(m)
+t.left(90)
+
+
+t.forward(n)
+t.left(90)
+
+t.forward(m)
+t.left(90)
+
+
+t.forward(n)
+t.left(90)
+
+turtle.done()
